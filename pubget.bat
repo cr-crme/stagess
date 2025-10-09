7d654d26b253202b20ac99e9ae1f588e6305e449
Pariterre <pariterre@hotmail.com>
2025-10-09 10:44:48 -0400

    Forced CRLF or LF depending on file extension

--- pubget.bat
+++ pubget.bat
@@ -1,26 +1,31 @@
-@REM This script navigates all the subdirectories of the project
-@REM and runs the command "pub get" in each of them.
+@echo off
+setlocal enabledelayedexpansion
+REM Prevent script from stopping on errors
+set "continue_on_error=true"
 
-@REM Currently translating the script from bash to batch
-@REM Usage: pubget.bat
-@REM This script is intended to be run from the root directory of the project.
+REM This script navigates all the subdirectories of the project
+REM and runs the command "pub get" in each of them.
 
-@REM Move to the script directory
+REM Currently translating the script from bash to batch
+REM Usage: pubget.bat
+REM This script is intended to be run from the root directory of the project.
+
+REM Move to the script directory
 cd /d "%~dp0"
 
-@REM @REM Pub get in the stagess_common directory
+REM Pub get in the stagess_common directory
 pushd stagess_common
 echo "Running pub get in stagess_common directory..."
-dart pub get
+call dart pub get
 popd
 
-@REM @REM Pub get in the stagess_common_flutter directory
+REM Pub get in the stagess_common_flutter directory
 pushd stagess_common_flutter
 echo "Running pub get in stagess_common_flutter directory..."
-dart pub get
+call dart pub get
 popd
 
-@REM Pub get in the external directory
+REM Pub get in the external directory
 pushd external\crcrme_material_theme
 echo "Running pub get in external\crcrme_material_theme directory..."
 call flutter pub get
@@ -37,7 +42,7 @@ echo "Running pub get in external\pdf_generation directory..."
 call flutter pub get
 popd
 
-@REM Pub get in the stagess_backend directory
+REM Pub get in the stagess_backend directory
 pushd stagess_backend
 echo "Running pub get in stagess_backend directory..."
 call dart pub get
@@ -47,13 +52,13 @@ call flutter pub get
 popd
 popd
 
-@REM Pub get in the stagess directory
+REM Pub get in the stagess directory
 pushd stagess
 echo "Running pub get in stagess directory..."
 call flutter pub get
 popd
 
-@REM Pub get in the stagess_admin directory
+REM Pub get in the stagess_admin directory
 pushd stagess_admin
 echo "Running pub get in stagess_admin directory..."
 call flutter pub get
