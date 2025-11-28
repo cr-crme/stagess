d5397e5d299a148c0c25e8d7396fe0c9585b18d1
Pariterre <pariterre@hotmail.com>
2025-11-28 12:02:11 -0500

    Continued C2 PDF

--- stagess/lib/screens/student/pages/pdf/attitude_pdf_template.dart
+++ stagess/lib/screens/student/pages/pdf/attitude_pdf_template.dart
@@ -28,16 +28,83 @@ Future<Uint8List> generateAttitudePdf(
 
   document.addPage(
     pw.Page(
-      build: (pw.Context context) => pw.Center(
+      build: (pw.Context context) => pw.Align(
+          alignment: pw.Alignment.topLeft,
           child: pw.Column(
               crossAxisAlignment: pw.CrossAxisAlignment.start,
               children: [
-            // TODO Rendu ici
-            _buildSectionTitle(
-                title: Politeness.title,
-                controller: controller,
-                elements: Politeness.values),
-          ])),
+                // TODO Rendu ici
+                _buildSectionTitle(
+                    title: '1. ${Inattendance.title}',
+                    controller: controller,
+                    elements: Inattendance.values),
+                pw.SizedBox(height: 16),
+                _buildSectionTitle(
+                    title: '2. ${Politeness.title}',
+                    controller: controller,
+                    elements: Politeness.values),
+
+                // _AttitudeRadioChoices(
+                //         title: '1. *${Inattendance.title}',
+                //         formController: widget.formController,
+                //         elements: Inattendance.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '2. *${Ponctuality.title}',
+                //         formController: widget.formController,
+                //         elements: Ponctuality.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '3. *${Sociability.title}',
+                //         formController: widget.formController,
+                //         elements: Sociability.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '4. *${Politeness.title}',
+                //         formController: widget.formController,
+                //         elements: Politeness.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '5. *${Motivation.title}',
+                //         formController: widget.formController,
+                //         elements: Motivation.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '6. *${DressCode.title}',
+                //         formController: widget.formController,
+                //         elements: DressCode.values,
+                //         editMode: widget.editMode,
+                //       ),
+                // _AttitudeRadioChoices(
+                //         title: '7. *${QualityOfWork.title}',
+                //         formController: widget.formController,
+                //         elements: QualityOfWork.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '8. *${Productivity.title}',
+                //         formController: widget.formController,
+                //         elements: Productivity.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '9. *${Autonomy.title}',
+                //         formController: widget.formController,
+                //         elements: Autonomy.values,
+                //         editMode: widget.editMode,
+                //       ),
+                //       _AttitudeRadioChoices(
+                //         title: '10. *${Cautiousness.title}',
+                //         formController: widget.formController,
+                //         elements: Cautiousness.values,
+                //         editMode: widget.editMode,
+                //       ),
+              ])),
     ),
   );
 
@@ -55,8 +122,37 @@ pw.Widget _buildSectionTitle({
           crossAxisAlignment: pw.CrossAxisAlignment.start,
           children: [
             pw.Text('$title :', style: _textStyleBold),
-            pw.Text(controller.responses[elements[0].runtimeType]!.toString(),
-                style: _textStyle)
+            ...elements.map((element) => pw.Padding(
+                padding: pw.EdgeInsets.only(top: 8),
+                child: pw.Row(
+                  crossAxisAlignment: pw.CrossAxisAlignment.center,
+                  mainAxisSize: pw.MainAxisSize.min,
+                  children: [
+                    pw.Container(
+                      width: 12,
+                      height: 12,
+                      decoration: pw.BoxDecoration(
+                        shape: pw.BoxShape.circle,
+                        border: pw.Border.all(color: PdfColors.black),
+                      ),
+                      child:
+                          controller.responses[element.runtimeType] == element
+                              ? pw.Center(
+                                  child: pw.Container(
+                                    width: 6,
+                                    height: 6,
+                                    decoration: pw.BoxDecoration(
+                                      shape: pw.BoxShape.circle,
+                                      color: PdfColors.black,
+                                    ),
+                                  ),
+                                )
+                              : pw.Container(),
+                    ),
+                    pw.SizedBox(width: 4),
+                    pw.Text(element.name, style: _textStyle),
+                  ],
+                ))),
           ],
         );
 }
