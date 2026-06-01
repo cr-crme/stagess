177c35ae1ff6f6fa61acb0587d48466e5b6155ab
Pariterre <pariterre@hotmail.com>
2026-06-01 16:12:49 -0400

    Added a way to zoom out/in in PDF viewer

--- stagess/lib/main_widget.dart
+++ stagess/lib/main_widget.dart
@@ -39,83 +39,80 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final visa = StudentVisa(
+        date: DateTime.now(),
+        form: VisaForm(
+            experiencesAndAptitudes: [
+              ExperiencesAndAptitudes(
+                  index: 0, isSelected: true, text: 'Experience 1'),
+              ExperiencesAndAptitudes(
+                  index: 1, isSelected: true, text: 'Experience 2'),
+            ],
+            attestationsAndMentions: [
+              AttestationsAndMentions(
+                  index: 0, isSelected: true, text: 'Attestation 1'),
+              AttestationsAndMentions(
+                  index: 1, isSelected: true, text: 'Attestation 2'),
+            ],
+            sstTrainings: [
+              SstTraining(
+                  index: 0,
+                  isSelected: true,
+                  trainingId: SstTraining.availableTrainings.keys.first,
+                  isHidden: false),
+              SstTraining(
+                  index: 1,
+                  isSelected: true,
+                  trainingId: SstTraining.availableTrainings.keys.elementAt(1),
+                  isHidden: false),
+            ],
+            isGatewayToFmsAvailable: false,
+            certificates: [
+              Certificate(
+                  index: 0,
+                  isSelected: true,
+                  certificateType: CertificateType.fpt)
+            ],
+            skills: [
+              Skill(
+                  index: 0,
+                  isSelected: true,
+                  skillId:
+                      job_service.ActivitySectorsService.allSkills.first.id)
+            ],
+            references: [
+              Reference(
+                  index: 0,
+                  isSelected: true,
+                  referee: 'Referee 1',
+                  enterprise: 'Enterprise 1',
+                  phoneNumber: PhoneNumber.fromString('123-456-7890'),
+                  email: 'referee1@example.com',
+                  supplementaryInfo: 'Reference 1 supplementary info'),
+            ],
+            forces: [
+              Attitude(
+                  index: 0,
+                  isSelected: true,
+                  attitudeId: Attitude.availableItems.keys.first)
+            ],
+            challenges: [
+              Attitude(
+                  index: 0,
+                  isSelected: true,
+                  attitudeId: Attitude.availableItems.keys.elementAt(1))
+            ],
+            successConditions: [
+              SuccessConditions(
+                  index: 0, isSelected: true, text: 'Success Condition 1')
+            ]),
+        formVersion: StudentVisa.currentVersion);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Stagess - Main Page')),
       body: Center(
         child: TextButton(
           onPressed: () {
-            final visa = StudentVisa(
-                date: DateTime.now(),
-                form: VisaForm(
-                    experiencesAndAptitudes: [
-                      ExperiencesAndAptitudes(
-                          index: 0, isSelected: true, text: 'Experience 1'),
-                      ExperiencesAndAptitudes(
-                          index: 1, isSelected: true, text: 'Experience 2'),
-                    ],
-                    attestationsAndMentions: [
-                      AttestationsAndMentions(
-                          index: 0, isSelected: true, text: 'Attestation 1'),
-                      AttestationsAndMentions(
-                          index: 1, isSelected: true, text: 'Attestation 2'),
-                    ],
-                    sstTrainings: [
-                      SstTraining(
-                          index: 0,
-                          isSelected: true,
-                          trainingId: SstTraining.availableTrainings.keys.first,
-                          isHidden: false),
-                      SstTraining(
-                          index: 1,
-                          isSelected: true,
-                          trainingId:
-                              SstTraining.availableTrainings.keys.elementAt(1),
-                          isHidden: false),
-                    ],
-                    isGatewayToFmsAvailable: false,
-                    certificates: [
-                      Certificate(
-                          index: 0,
-                          isSelected: true,
-                          certificateType: CertificateType.fpt)
-                    ],
-                    skills: [
-                      Skill(
-                          index: 0,
-                          isSelected: true,
-                          skillId: job_service
-                              .ActivitySectorsService.allSkills.first.id)
-                    ],
-                    references: [
-                      Reference(
-                          index: 0,
-                          isSelected: true,
-                          referee: 'Referee 1',
-                          enterprise: 'Enterprise 1',
-                          phoneNumber: PhoneNumber.fromString('123-456-7890'),
-                          email: 'referee1@example.com',
-                          supplementaryInfo: 'Reference 1 supplementary info'),
-                    ],
-                    forces: [
-                      Attitude(
-                          index: 0,
-                          isSelected: true,
-                          attitudeId: Attitude.availableItems.keys.first)
-                    ],
-                    challenges: [
-                      Attitude(
-                          index: 0,
-                          isSelected: true,
-                          attitudeId: Attitude.availableItems.keys.elementAt(1))
-                    ],
-                    successConditions: [
-                      SuccessConditions(
-                          index: 0,
-                          isSelected: true,
-                          text: 'Success Condition 1')
-                    ]),
-                formVersion: StudentVisa.currentVersion);
-
             showPdfDialog(context,
                 pdfGeneratorCallback: (context, format) => generateVisaPdf(
                     context, format,
--- stagess_common_flutter/lib/widgets/dialogs/show_pdf_dialog.dart
+++ stagess_common_flutter/lib/widgets/dialogs/show_pdf_dialog.dart
@@ -10,34 +10,131 @@ void showPdfDialog(
           BuildContext context, PdfPageFormat format)
       pdfGeneratorCallback,
 }) async {
+  final data = await pdfGeneratorCallback(context, PdfPageFormat.letter);
+  if (!context.mounted) return;
+
   return showDialog(
       context: context,
       builder: (ctx) => Dialog(
-            backgroundColor: Colors.grey[700],
+            backgroundColor: Colors.grey[500]?.withAlpha(225),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               children: [
-                Align(
-                    alignment: Alignment.topRight,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 12.0),
-                      child: IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: () => Navigator.of(ctx).pop(),
+                Stack(
+                  children: [
+                    Container(
+                      height: 48,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[700],
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(25),
+                        ),
                       ),
-                    )),
-                Expanded(
-                  child: PdfPreview(
-                    allowPrinting: true,
-                    allowSharing: true,
-                    canChangeOrientation: false,
-                    canChangePageFormat: false,
-                    canDebug: false,
-                    initialPageFormat: PdfPageFormat.letter,
-                    build: (format) => pdfGeneratorCallback(context, format),
-                  ),
+                    ),
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 12.0),
+                          child: IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () => Navigator.of(ctx).pop(),
+                          ),
+                        )),
+                  ],
                 ),
+                Expanded(child: _PdfViewer(pdfData: data)),
               ],
             ),
           ));
+}
+
+class _PdfViewer extends StatefulWidget {
+  const _PdfViewer({required this.pdfData});
+  final Uint8List pdfData;
+
+  @override
+  State<_PdfViewer> createState() => _PdfViewerState();
+}
+
+class _PdfViewerState extends State<_PdfViewer> {
+  bool isInitialized = false;
+  double screenWidth = -1.0;
+  double paddingController = double.infinity;
+  double zoomSpeed = 0.0;
+
+  late final pdfWidget = PdfPreview(
+    allowPrinting: true,
+    allowSharing: true,
+    canChangeOrientation: false,
+    canChangePageFormat: false,
+    canDebug: false,
+    initialPageFormat: PdfPageFormat.letter,
+    build: (format) => Future.value(widget.pdfData),
+  );
+
+  void _handleScreenSizeChange() {
+    screenWidth = MediaQuery.of(context).size.width;
+    zoomSpeed = screenWidth * 0.05;
+    _limitZoomOut();
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  void _handleZoomIn() {
+    paddingController -= zoomSpeed;
+    if (paddingController < 0) {
+      paddingController = 0;
+    }
+    setState(() {});
+  }
+
+  void _handleZoomOut() {
+    paddingController += zoomSpeed;
+    _limitZoomOut();
+    setState(() {});
+  }
+
+  void _limitZoomOut() {
+    final minimumPdfWidth =
+        isInitialized ? MediaQuery.of(context).size.width / 5 : 300;
+    isInitialized = true;
+    if (paddingController >
+        MediaQuery.of(context).size.width / 2 - minimumPdfWidth) {
+      paddingController =
+          MediaQuery.of(context).size.width / 2 - minimumPdfWidth;
+    }
+    if (paddingController < 0) paddingController = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (screenWidth != MediaQuery.of(context).size.width) {
+      _handleScreenSizeChange();
+    }
+
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: paddingController),
+          child: pdfWidget,
+        ),
+        Positioned(
+          bottom: 16,
+          right: 16,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: _handleZoomIn,
+                child: Icon(Icons.zoom_in),
+              ),
+              SizedBox(height: 12),
+              FloatingActionButton(
+                onPressed: _handleZoomOut,
+                child: Icon(Icons.zoom_out),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
